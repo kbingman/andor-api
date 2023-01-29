ce7ab945b451fe4a773083614a432b9c996281c8
Keith Bingman <keith@keithbingman.com>
2023-01-28 21:36:39 -0800

    fixup! Updating Episode and People APIs

--- crates/db_adapter/src/lib.rs
+++ crates/db_adapter/src/lib.rs
@@ -1,9 +1,12 @@
 use anyhow::Result;
 
-pub trait DbAdapter<Model, Response> {
-    fn insert(&self, model: &Model) -> Result<Response>;
-    fn find_all(&self) -> Result<Response>;
-    fn find_one(&self, id: i32) -> Result<Response>;
-    fn update(&self, id: i32, model: &Model) -> Result<Response>;
+/// A Trait for connect arbitrary databases with CRUD actions
+/// This connect a Model struct to a given DB and describes a
+/// generic interface for connecting to the DB
+pub trait DbAdapter<Model> {
+    fn insert(&self, model: &Model) -> Result<Option<Model>>;
+    fn find_all(&self) -> Result<Vec<Model>>;
+    fn find_one(&self, id: i32) -> Result<Option<Model>>;
+    fn update(&self, id: i32, model: &Model) -> Result<Option<Model>>;
     fn delete(&self, id: i32) -> Result<u64>;
 }
